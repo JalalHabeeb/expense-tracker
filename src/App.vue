7aa870d40ead5faf165b267de19fa40ef193769e
Jalal Habeeb <jalalhabeeb@gmail.com>
2024-02-10 11:16:24 +0100

    add components

--- src/App.vue
+++ src/App.vue
@@ -1,1 +1,27 @@
-<template>My App</template>
+<template>
+  <Header />
+  <div class="container">
+    <Balance />
+    <IncomeExpenses />
+    <TransactionList />
+    <AddTransactions />
+  </div>
+</template>
+
+<script>
+import Header from "./components/Header.vue";
+import Balance from "./components/Balance.vue";
+import IncomeExpenses from "./components/IncomeExpenses.vue";
+import TransactionList from "./components/TransactionList.vue";
+import AddTransactions from "./components/AddTransactions.vue";
+
+export default {
+  components: {
+    Header,
+    Balance,
+    IncomeExpenses,
+    TransactionList,
+    AddTransactions,
+  },
+};
+</script>
